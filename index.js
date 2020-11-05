fe8ae520297a5cbdc5f234a7d9d5b113a21ceed1
Brice Chkir <brice.chkir@outlook.fr>
2020-11-05 11:27:23 +0100

    Set up the nodejs server and added the departements list API

--- index.js
+++ index.js
@@ -1,24 +1,111 @@
 // Chargement du module http
 const http = require('http');
+const fs = require("fs");
+const path = require('path');
 // Definition du port d'écoute
 const port = 80;
+const host = 'd4g-16.bricechk.fr';
 /*
     Création du serveur (createServer)
     Fonction qui gère les requetes
  */
 
 const server = http.createServer((req, res) => {
-    //Réponse du serveur
-    res.end('Mon premier serveur nodejs!!!');
+    var url = req.url;
+    var req_path = decodeURI(url.replace(/^\/+/, "").replace(/\?.*$/, ""));
+    res.writeHead(200);
+
+    var params = new URLSearchParams(url.replace(/^.*\?/, ""));
+    var searchObj = JSON.parse('{"' + decodeURI(params).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+
+    if (req_path === "search") {
+
+    } else if (req_path === "depts") {
+        if ('region' in searchObj) {
+            let r = {};
+            r.departements = [];
+            let bdd = getBdd();
+            let nomRegion = searchObj['region'];
+            if (nomRegion in bdd) {
+                let region = bdd[nomRegion];
+                let depts = region.departements;
+                Object.keys(depts).forEach(k => {
+                    r.departements.push(k);
+                });
+
+                r.score_global = region.score_global;
+                r.indice_acces_info = region.indice_acces_info;
+                r.indice_acces_interf_num = region.indice_acces_interf_num;
+                r.indice_competences_admin = region.indice_competences_admin;
+                r.indice_competences_num = region.indice_competences_num;
+
+                res.end(JSON.stringify(r));
+
+            } else {
+                //TODO msg erreur region non trouvée
+            }
+        } else {
+            //TODO msg erreur
+        }
+    } else if (req_path === "coms") {
+
+    } else if (req_path === "pdf") {
+
+    } else {
+        var filePath = '.' + req.url;
+        if (filePath == './')
+            filePath = './index.html';
+
+        var extname = path.extname(filePath);
+        var contentType = 'text/html';
+        switch (extname) {
+            case '.js':
+                contentType = 'text/javascript';
+                break;
+            case '.css':
+                contentType = 'text/css';
+                break;
+            case '.json':
+                contentType = 'application/json';
+                break;
+            case '.png':
+                contentType = 'image/png';
+                break;
+            case '.jpg':
+                contentType = 'image/jpg';
+                break;
+            case '.wav':
+                contentType = 'audio/wav';
+                break;
+        }
+
+        fs.readFile(filePath, function(error, content) {
+            if (error) {
+                if(error.code == 'ENOENT'){
+                    fs.readFile('./404.html', function(error, content) {
+                        res.writeHead(200, { 'Content-Type': contentType });
+                        res.end(content, 'utf-8');
+                    });
+                }
+                else {
+                    res.writeHead(500);
+                    res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                    res.end();
+                }
+            }
+            else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content, 'utf-8');
+            }
+        });
+    }
 });
 
 // connection du serveur au port d'ecoute
-server.listen(port, (err) => {
-    //check si il y'a pas une erreur pendant le lancement du serveur
-    if (err) {
-        //Afficher l'erreur survenue pendant le lancement
-        return console.log('Erreur: ', err)
-    }
-    // Afficher l'adresse sur serveur
-    console.log(`Serveur: localhost:${port}`)
+server.listen(port, host, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
+
+function getBdd() {
+    return JSON.parse(fs.readFileSync('bdd.json'));
+}
